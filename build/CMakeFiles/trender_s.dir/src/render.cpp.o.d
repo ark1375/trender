86c6950a66a0ab45c09673fdcaf0f64b73f6f9fd
ark1375 <ark.blackhat@gmail.com>
2024-02-20 20:24:09 +0330

    Intialize repo

---
+++ build/CMakeFiles/trender_s.dir/src/render.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/trender_s.dir/src/render.cpp.o: \
+ /home/oracle/Projects/cpp_course/trend_s/src/render.cpp \
+ /usr/include/stdc-predef.h
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "../lib/gmtl/Matrix.h"
+#include "../lib/gmtl/MatrixOps.h"
+#include "../lib/gmtl/Output.h"
+#include "../lib/gmtl/Vec.h"
+#include "../lib/gmtl/VecOps.h"
+#include "../lib/gmtl/Xforms.h"
+
+int main(){
+
+}
+
